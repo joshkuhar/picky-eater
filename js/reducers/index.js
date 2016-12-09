5da0bbd75833c62523c550b0d01e7226b3856328
Josh Kuhar <jdkuhar@gmail.com>
2016-12-09 02:00:24 -0500

    added store and reducers for AJAX calls

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -3,17 +3,22 @@ var Data = require('../data');
 
 var initialState = {
 	restaurant: Data.locations.pizza,
-	index: 1
+	index: 1,
+	places: "",
+	data: Data
 };
 
 var locationReducer = function(state, action) {
 	state = state || initialState;
-	console.log(state);
 	if (action.type === actions.CHANGE_LOCATION) {
-		var place = Object.keys(Data.locations);
-		var name = Data.locations[place[state.index % place.length]];
 		var index = state.index + 1;
-		return {restaurant: name, index: index}
+		return {restaurant: action.location, index: index, data: state.data}
+	} 
+	else if (action.type === actions.GET_LOCATIONS) {
+		return {places: action.locations, data: Data, address: "I am the placeholder address, woooo"}
+	} //
+	else if (action.type === actions.FETCH_SUCCESS) {
+		return {data: action.data}
 	}
 	return state;
 };
---
+++ js/store.js
@@ -0,0 +1,9 @@
+var redux = require('redux');
+var createStore = redux.createStore;
+var applyMiddleware = redux.applyMiddleware;
+var thunk = require('redux-thunk').default;
+
+var reducers = require('./reducers/index');
+
+var store = createStore(reducers.locationReducer, applyMiddleware(thunk));
+module.exports  = store;
