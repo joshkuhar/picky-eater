f15d606369494d4fda9de214440aa546c4fe524c
Josh Kuhar <jdkuhar@gmail.com>
2016-12-14 19:27:39 -0500

    too be honest, not sure what i did

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,17 +1,15 @@
 require('isomorphic-fetch');
-var Calls = require('./api-Calls');
-var Parsed = require('./functions');
 
-var CHANGE_LOCATION = 'CHANGE_LOCATION';
-var changeLocation = function(location){
+var GET_OFFER = 'GET_OFFER';
+var getOffer = function(location){
 	return {
-		type: CHANGE_LOCATION,
+		type: GET_OFFER,
 		location: location
 		};
 };
 
-exports.CHANGE_LOCATION = CHANGE_LOCATION;
-exports.changeLocation = changeLocation;
+exports.GET_OFFER = GET_OFFER;
+exports.getOffer = getOffer;
 
 var GET_LOCATIONS = 'GET_LOCATIONS';
 var getLocations = function(locations) {
@@ -35,11 +33,46 @@ var fetchSuccess = function(data) {
 exports.FETCH_SUCCESS = FETCH_SUCCESS;
 exports.fetchSuccess = fetchSuccess;
 
+var FETCH_LOCATION_SUCCESS = 'FETCH_LOCATION_SUCCESS';
+var fetchLocationSuccess = function(data){
+    console.log('locationSucess ', data);
+    return {
+        type: FETCH_LOCATION_SUCCESS,
+        address: data.location.address,
+        name: data.name,
+        rating: data.rating
+    }
+};
+
+exports.FETCH_LOCATION_SUCCESS = FETCH_LOCATION_SUCCESS;
+exports.fetchLocationSuccess = fetchLocationSuccess;
+
+var COUNTDOWN = 'COUNTDOWN';
+var countDown = function(){
+        return { type: COUNTDOWN }   
+    };
 
 
-var fetchLocations = function() {
+
+exports.COUNTDOWN = COUNTDOWN;
+exports.countDown = countDown;
+
+var fetchLocations = function(id) {
+    var p = {
+        url: "https://api.foursquare.com/v2/venues/search?",
+        clId: "client_id=LT5PEZMUYPGXVYMZX0BDR1O2001DFRSKWV2DWI3AFFEPDWJZ",
+        secret: "&client_secret=MYGCCF0ENAGRFLGAUKMOKYFIDJGULKUW0V0Q3UENAWVO2R2P",
+        v: "&v=20161201",
+        near: "&near=philadelphia,pa",
+        categoryId: "&categoryId=",
+        limit: "&limit=50",
+        radius: "&radius=3000",
+        m: "&m=foursquare"
+    };
+    var play = p.url + p.clId + p.secret + p.v + p.near + p.categoryId + id + p.limit + p.radius + p.m;
+
     return function(dispatch) {
-        return fetch(Calls.play).then(function(response) {
+        return fetch(play).then(function(response) {
             if (response.status < 200 || response.status >= 300) {
                 var error = new Error(response.statusText)
                 error.response = response
@@ -51,7 +84,7 @@ var fetchLocations = function() {
             return response.json();
         })
         .then(function(data) {
-            Parsed.parsed(data);
+            //Parsed.parsed(data);
             return dispatch(
                 fetchSuccess(data)
             );
@@ -63,6 +96,42 @@ var fetchLocations = function() {
 };
 
 exports.fetchLocations = fetchLocations;
+
+var fetchSingleLocation = function(id) {
+    var p = {
+        url: "https://api.foursquare.com/v2/venues/",
+        clId: "&client_id=LT5PEZMUYPGXVYMZX0BDR1O2001DFRSKWV2DWI3AFFEPDWJZ",
+        secret: "&client_secret=MYGCCF0ENAGRFLGAUKMOKYFIDJGULKUW0V0Q3UENAWVO2R2P",
+        v: "v=20161201",
+        m: "&m=foursquare"
+    };
+    var play = p.url + id + "?"  + p.v + p.m + p.clId + p.secret;
+
+    return function(dispatch) {
+        return fetch(play).then(function(response) {
+            if (response.status < 200 || response.status >= 300) {
+                var error = new Error(response.statusText)
+                error.response = response
+                throw error;
+            }
+            return response;
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.response.venue);
+            return dispatch(
+                fetchLocationSuccess(data.response.venue)
+            );
+        })
+        .catch(function(error) {
+           console.log(error);
+        });
+    }
+};
+
+exports.fetchSingleLocation = fetchSingleLocation;
 
 
 
