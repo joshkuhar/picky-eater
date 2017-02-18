e452569a4959eb6199e72c38fec8ba99822dfc95
Josh Kuhar <jdkuhar@gmail.com>
2017-02-18 15:18:29 -0500

    changed link styles

--- js/components/app.js
+++ js/components/app.js
@@ -9,7 +9,7 @@ var App = React.createClass({
 	return(
 		<div>
 			<div className="main-photo">
-				<h2 className="headline">Restaraunt Picker</h2>
+				<h2 className="headline">Picky Eater</h2>
 				<div className="nav-bar">
 					<Link to={"/locations/instructions"} ><div className="nav-bar-item">Instructions</div></Link>
 					<Link to={"/locations"} ><div className="nav-bar-item">Home</div></Link>
